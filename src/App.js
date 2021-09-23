86f878d48a11185f215bfde2e9f6f7bd5804c3cb
Madmonkey08 <ajtm082698@gmail.com>
2021-09-23 18:46:25 -0500

    Avances en columna izquierda, formulario de inicio de sesión

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import { Login } from './components/Login';
 
 function App() {
   return (
-    <div className='App'>
-      <h1> HOLA MUNDO DESDE REACT</h1>
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styles/login.css';
+
+export const Login = () => {
+    return (
+        <section id="login">
+        <div id="col-izq">
+            <h3>Bienvenido a</h3>
+            <h1 id="titulo-principal">ST Masters</h1>
+            <form id="formulario" action="">
+                <label htmlFor="usuario">Usuario:</label>
+                <input type="text" placeholder="Ingresa tu nombre de usuario"/>
+                <label htmlFor="contraseña">Contraseña:</label>
+                <input type="password" placeholder="Ingresa tu contraseña"/>
+                <div id="ayudas">
+                <div id="checkbox">
+                    <input type="checkbox" name="recordar" id="recordar" /> <spam id="texto-checkbox">Recuérdame</spam>
+                </div>
+                <div>
+                    <a id="olvido-contraseña" href="https://www.instagram.com/mjtm_1505/">Olvidé mi contraseña</a>
+                </div>
+                </div>
+                <button id="boton-login" type="submit">Iniciar sesión</button>
+            </form>
+        </div>
+        <div id="col-der">
+        <h1>Hey</h1>
+        </div>
+        </section>
+    );
+}
