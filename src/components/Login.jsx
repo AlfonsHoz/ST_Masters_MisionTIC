27abbba4dbec7891cef5c7b572b5e683932c34ce
Daniel Solarte <solartemarindanielfelipe@gmail.com>
2021-09-27 07:57:07 -0500

    Vista Login terminada, se termino la columna derecha que estaba faltando

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import '../styles/login.css';
+import React from "react";
+import "../styles/login.css";
+import logo from "../assets/img/logo.svg";
 
 export const Login = () => {
-    return (
-        <section id="login">
-        <div id="col-izq">
-            <h3>Bienvenido a</h3>
-            <h1 id="titulo-principal">ST Masters</h1>
-            <form id="formulario" action="">
-                <label htmlFor="usuario">Usuario:</label>
-                <input type="text" placeholder="Ingresa tu nombre de usuario"/>
-                <label htmlFor="contraseña">Contraseña:</label>
-                <input type="password" placeholder="Ingresa tu contraseña"/>
-                <div id="ayudas">
-                <div id="checkbox">
-                    <input type="checkbox" name="recordar" id="recordar" /> <span id="texto-checkbox">Recuérdame</span>
-                </div>
-                <div>
-                    <a id="olvido-contraseña" href="#">Olvidé mi contraseña</a>
-                </div>
-                </div>
-                <button id="boton-login" type="submit">Iniciar sesión</button>
-            </form>
+  return (
+    <section id="login">
+      <div id="col-izq">
+        <div id="content-izq">
+          <h3>Bienvenido a</h3>
+          <h1 id="titulo-principal">ST Masters</h1>
+          <form id="formulario" action="">
+            <label htmlFor="usuario">Usuario:</label>
+            <input type="text" placeholder="Ingresa tu nombre de usuario" />
+            <label htmlFor="contraseña">Contraseña:</label>
+            <input type="password" placeholder="Ingresa tu contraseña" />
+            <div id="ayudas">
+              <div id="checkbox">
+                <input type="checkbox" name="recordar" id="recordar" />{" "}
+                <span id="texto-checkbox">Recuérdame</span>
+              </div>
+              <a id="olvido-contraseña" href="#">
+                Olvidé mi contraseña
+              </a>
+            </div>
+            <button id="boton-login" type="submit">
+              Iniciar sesión
+            </button>
+          </form>
         </div>
-        <div id="col-der">
-            <h2>Guerrerooo guerreroo</h2>
-        </div>
-        </section>
-    );
-}
+      </div>
+      <div id="col-der">
+        <img id="logo" src={logo}></img>
+        <h1>Manufacturing SAS</h1>
+      </div>
+    </section>
+  );
+};
