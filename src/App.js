df45023bcab38170d3d692392c8593f1fb023bfa
Madmonkey08 <ajtm082698@gmail.com>
2021-09-28 21:23:33 -0500

    Propuesta de Navbar y header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Login } from './components/Login';
+import Navbar from './components/Navbar';
+import { ConsultarProducto } from './components/ConsultarProducto';
 import { ListadoUsuarios } from './components/ListadoUsuarios';
 
 function App() {
   return (
     <>
+      <Navbar />
+      <ConsultarProducto />
       <ListadoUsuarios />
     </>
   );
--- src/components/ConsultarProducto.jsx
+++ src/components/ConsultarProducto.jsx
@@ -4,7 +4,7 @@ import '../styles/consultarProducto.css';
 export const ConsultarProducto = () => {
     return (
         <div id="contenedorConsulta">  
-            <h2>Consultar productos</h2>
+            <h2 id="titulo">Consultar productos</h2>
             <label htmlFor="buscarProducto">Buscar producto:</label>
             <input id="campoBuscar" name="buscarProducto" type="text" placeholder="Ingresa el código o nombre del producto"/>
         </div>
