51161fdb74c28398baf6478cdf2a5ea4093d7ed8
Daniel Solarte <solartemarindanielfelipe@gmail.com>
2021-09-28 21:51:07 -0500

    Componente EditarUsuarios terminado, login resposive aun en proceso

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Login } from './components/Login';
+import "bootstrap/dist/css/bootstrap.min.css";
+import EditarUsuarios from "./components/EditarUsuarios.jsx";
 
 function App() {
   return (
     <>
-      <Login />
+      <EditarUsuarios />
     </>
   );
 }
