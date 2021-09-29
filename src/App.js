7b24568d4e1e180de7f1f8403270f8dde01292c9
Ismael Alexander Carvajal González <Ismaelcarvajal@outlook.com>
2021-09-28 19:19:52 -0500

    Me permito subir la página de gestionar ventas haciendo la anotación de que no pude arreglar el Header para que la letra quede encima de la imagen de tal forma que no se oscurezca

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Listadoventas from "./components/Listadoventas";
 import Navbar from "./components/Navbar.jsx";
+import GestionarVentas from "./components/Gestionarventas.jsx";
+import Consultarventas from "./components/Consultarventas";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <GestionarVentas/>
+      <Consultarventas/>
       <Listadoventas />
     </div>
   );
