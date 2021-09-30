c9bfb28cc7ce0ddfcb8666500a7706a13692b5f0
Ismael Alexander Carvajal González <Ismaelcarvajal@outlook.com>
2021-09-30 11:15:42 -0500

    Me permito subir el componente de listado Productos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import Listadoventas from "./components/Listadoventas";
-import Navbar from "./components/Navbar.jsx";
-import GestionarVentas from "./components/Gestionarventas.jsx";
-import Consultarventas from "./components/Consultarventas";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <GestionarVentas/>
-      <Consultarventas/>
-      <Listadoventas />
+
     </div>
   );
 }
