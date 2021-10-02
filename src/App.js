14c6803231607d5f36cd92bdb4cbebf8510c334d
Ismael Alexander Carvajal González <Ismaelcarvajal@outlook.com>
2021-10-02 17:18:48 -0500

    Me permito subir cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import EditarVentas from "./components/EditarVentas"
+import ListadoProductos from "./components/Listadoproductos";
 
 
 function App() {
   return (
     <div className="App">
-
+    <EditarVentas />
+    <ListadoProductos />
     </div>
   );
 }
