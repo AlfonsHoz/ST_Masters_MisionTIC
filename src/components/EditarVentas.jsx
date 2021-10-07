eb84f5e676e3c4405367eb356e3c894bcbf6638b
Madmonkey08 <ajtm082698@gmail.com>
2021-10-06 23:33:39 -0500

    Corrigiendo boton agregar producto

--- src/components/EditarVentas.jsx
+++ src/components/EditarVentas.jsx
@@ -20,67 +20,65 @@ const EditarVentas = ({ props }) => {
       <div>
         <h2 id="act-ven">Detalle de venta </h2>
         <div id="primerDiv">
-        <label id="cod-ven">
-          Código venta <br />
-          <input
-            type="number"
-            id="ing-cod"
-            placeholder="Ingrese código venta"
-          ></input>
-          <p></p>
-        </label>
-        <div id="eti-ID">
-          <label id="id-ven">
-            ID Vendedor: <br />
+          <label id="cod-ven">
+            Código venta <br />
             <input
               type="number"
-              id="number"
-              placeholder="Ingrese el id"
+              id="ing-cod"
+              placeholder="Ingrese código venta"
+            ></input>
+            <p></p>
+          </label>
+          <div id="eti-ID">
+            <label id="id-ven">
+              ID Vendedor: <br />
+              <input
+                type="number"
+                id="number"
+                placeholder="Ingrese el id"
+              ></input>
+            </label>
+          </div>
+          <label id="nom-ven">
+            Nombre vendedor: <br />
+            <input
+              type="text"
+              id="ingr-nom"
+              placeholder="Ingrese el nombre"
             ></input>
           </label>
         </div>
-        <label id="nom-ven">
-          Nombre vendedor: <br />
-          <input
-            type="text"
-            id="ingr-nom"
-            placeholder="Ingrese el nombre"
-          ></input>
-        </label>
-      </div>
       </div>
       <div id="segundoDiv">
-      <div id="botones">
-        <label id="prod">
-          Producto: <br />
-          <input
-            ref={refProd}
-            type="text"
-            id="ingr-prod"
-            placeholder="Ingresa el código o nombre"
-          />
-        </label>
-        <label id="cant">
-          Cantidad: <br />
-          <input
-            ref={refCant}
-            type="number"
-            id="ingr-cant"
-            placeholder="Ingresa la cantidad"
-          />
-        </label>
+        <div id="botones">
+          <label id="prod">
+            Producto: <br />
+            <input
+              ref={refProd}
+              type="text"
+              id="ingr-prod"
+              placeholder="Ingresa el código o nombre"
+            />
+          </label>
+          <label id="cant">
+            Cantidad: <br />
+            <input
+              ref={refCant}
+              type="number"
+              id="ingr-cant"
+              placeholder="Ingresa la cantidad"
+            />
+          </label>
         </div>
         <div id="tercerDiv"></div>
-        <Link to={"/ventas/consultar"}>
         <button type="submit" className="agre-prod">
           {" "}
           Agregar producto{" "}
         </button>
-        </Link>
         <button type="submit" className="boton-actualizar">
           Actualizar
         </button>
-        </div>
+      </div>
     </div>
   );
 };
