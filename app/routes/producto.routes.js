351c6a08bdb077a8aaed022fd92d91a38a29d76b
Madmonkey08 <ajtm082698@gmail.com>
2021-10-10 14:53:08 -0500

    Agregando cambios

--- app/routes/producto.routes.js
+++ app/routes/producto.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
 
-const {obtenerProductos, crearProducto, actualizarProducto, eliminarProducto} = require('../controller/producto.controller');
+const { obtenerProductos, crearProducto, actualizarProducto, eliminarProducto } = require('../controller/producto.controller');
 
 const { validarCampos } = require('../middlewares/validar_campos');
 
@@ -29,18 +29,19 @@ router.put(
     `/${path}/:id`,
     [ // Middlewares
         check('codigo_producto', 'El código del producto es obligatorio.').not().isEmpty(),
+        check('codigo_producto', 'El código del producto debe tener al menos 4 caracteres.').isLength({ min: 4 }),
         check('nombre_producto', 'El nombre del producto es obligatorio.').not().isEmpty(),
+        check('nombre_producto', 'El nombre del producto debe tener al menos 4 caracteres.').isLength({ min: 4 }),
         check('precio_unitario', 'El precio unitario del producto es obligatorio.').not().isEmpty(),
         check('precio_unitario', 'El precio unitario debe ser un valor numérico.').isNumeric(),
-        check('cantidad', 'La cantidad del producto es obligatorio.').not().isEmpty(),
-        check('cantidad', 'La cantidad debe ser un valor numérico.').isNumeric(),
+        check('precio_unitario', 'El precio unitario debe tener al menos 3 caracteres.').isLength({ min: 3 }),
         validarCampos
     ],
     actualizarProducto);
 
 
 router.delete(
-    `/${path}/:id`, 
+    `/${path}/:id`,
     eliminarProducto),
 
-module.exports = router;
+    module.exports = router;
