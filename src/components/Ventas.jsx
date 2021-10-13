c7635cd5101d77a38de0207bc265e3f66ec5b4ef
AlfonsHoz <alfonsleo123@gmail.com>
2021-10-12 21:04:09 -0500

    Agregando la integración del back en listado de ventas

--- src/components/Ventas.jsx
+++ src/components/Ventas.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import ojo from "../assets/img/ojo.svg";
-import borrar from "../assets/img/eliminar.svg"
-import { Link } from 'react-router-dom'
+import borrar from "../assets/img/eliminar.svg";
+import { Link } from "react-router-dom";
 
 const Ventas = ({ props }) => {
-  const { fecha, codigo, total, vendedor } = props;
+  const { fecha_venta, codigo, total, vendedor } = props;
   return (
-      <tr>
-        <td>{fecha}</td>
-        <td>{codigo}</td>
-        <td>{total}</td>
-        <td>{vendedor}</td>
-        <td>
-          <Link
-            to={"/ventas/editar"}>
-            <img src={ojo} alt="" />
-          </Link>
+    <tr>
+      <td>{fecha_venta}</td>
+      <td>{codigo}</td>
+      <td>{total}</td>
+      <td>{vendedor}</td>
+      <td>
+        <Link to={"/ventas/editar"}>
+          <img src={ojo} alt='' />
+        </Link>
 
-          <img src={borrar} alt="" />
-        </td>
-      </tr>
+        <img src={borrar} alt='' />
+      </td>
+    </tr>
   );
 };
 
