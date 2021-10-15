0ff319aaec7fadf18a5341a1ea1af11965529c51
ismaelc511 <ismaelcarvajal@outlook.com>
2021-10-15 17:27:51 -0500

    Actualizo mi rama

--- src/helper/fetch.js
+++ src/helper/fetch.js
@@ -8,8 +8,11 @@ const axiosPetition = async (endpoint, data = {}, method = 'GET') => {
     const options = {
         method,
         url: `${baseUrl}${endpoint}`,
-        headers: { 'Content-Type': 'application/json' },
-        data
+        //headers: { 'Content-Type': 'application/json' },
+        data,
+        headers: {
+            Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkEzcV95WHBTYW5VLUNlQ1g0YkFndCJ9.eyJpc3MiOiJodHRwczovL3N0LW1hc3RlcnMudXMuYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA3OTcyNjIyMjU4MjQ5NDk3OTY4IiwiYXVkIjpbImFwaS1hdXRlbnRpY2FjaW9uLXN0LW1hc3RlcnMiLCJodHRwczovL3N0LW1hc3RlcnMudXMuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTYzNDMzNTg3NCwiZXhwIjoxNjM0NDIyMjc0LCJhenAiOiJobkk3cVpIZkcyUzdxMDNGMEJUQjF3YklsS1NWR3N5bSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ.F_DPC0TA11ALKorrJkrD7DXxzoy4vFVP-9MNk00Sf5NqW84rBPzYcMijB8es5c2xkevLKIn8P8er--MxwtpvqAkC2cPbzfNdMTjiv4LynVMF1U2L8vUq1uDmxfuwBr488aNmlXGuX5xPre0CGZKAoDbugLeSEfA6vqazxeUD5YnjzBG3e99Y9oisP3kPuxpHRHmv2GtSN49QBPqfQE3EH1ygHEMVR2yduTA9z2q__CxLBRGQybLPSq7wMr8ubI3g1cpxVDHIVru993T48yiXw9Ah4yFHXSbZwrDw82n7nezfLV_YIfupn8JZjITQiY39CB2sP3PWRFxojOqRTRseKw'
+          }
     }
 
     await axios
