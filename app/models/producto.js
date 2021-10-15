70beaa7b516a2e09a400a5bdde3c6ec06f23cce0
AlfonsHoz <alfonsleo123@gmail.com>
2021-10-14 19:57:21 -0500

    Agregando el estado al schema de producto

--- app/models/producto.js
+++ app/models/producto.js
@@ -1,23 +1,28 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const productoSchema = new Schema({
+const productoSchema = new Schema(
+  {
     codigo_producto: {
-        type: String,
-        required: [true, 'El código del producto es obligatorio.'],
-        unique: true
+      type: String,
+      required: [true, "El código del producto es obligatorio."],
+      unique: true,
     },
     nombre_producto: {
-        type: String,
-        required: [true, 'El nombre del producto es obligatorio.']
+      type: String,
+      required: [true, "El nombre del producto es obligatorio."],
     },
     precio_unitario: {
-        type: Number,
-        required: [true, 'El precio unitario es obligatorio.']
-    }
-
-},
-    {
-        versionKey: false
-    });
+      type: Number,
+      required: [true, "El precio unitario es obligatorio."],
+    },
+    estado: {
+      type: String,
+      required: [true, "El estado es obligatorio."],
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 module.exports = model("producto", productoSchema);
