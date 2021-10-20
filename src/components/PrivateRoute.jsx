190726fb6e7474f788fcc39fd869a16232583cb8
ismaelc511 <ismaelcarvajal@outlook.com>
2021-10-19 21:59:47 -0500

    Actualizar

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -45,7 +45,7 @@ const PrivateRoute = ({ children }) => {
 
     }
     if (isLoading) return <div>Loading...</div>;
-  }, [isAuthenticated, getAccessTokenSilently, setUserData]);
+  }, [isAuthenticated, getAccessTokenSilently, UserData]);
 console.log(isAuthenticated)
   if (!isAuthenticated) {
     return "https://stmasters.herokuapp.com/";
