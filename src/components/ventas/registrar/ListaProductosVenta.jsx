a92c724fb08c24b5d52cc95ffd1e619f2ae2ee1b
Madmonkey08 <ajtm082698@gmail.com>
2021-10-28 12:21:02 -0500

    Camabiando detalles en ventas

--- src/components/ventas/registrar/ListaProductosVenta.jsx
+++ src/components/ventas/registrar/ListaProductosVenta.jsx
@@ -4,15 +4,25 @@ import eliminar from "../../../assets/img/eliminar.svg";
 export const ListaProductosVenta = ({ props }) => {
   const { codigo_producto, nombre_producto, precio_unitario, cantidad } = props;
   return (
-    <tr>
-      <td>{codigo_producto}</td>
-      <td>{nombre_producto}</td>
-      <td>{precio_unitario}</td>
-      <td>{cantidad}</td>
-      <td>{precio_unitario * cantidad}</td>
-      <td>
-        <img src={eliminar} alt='' />
-      </td>
-    </tr>
+    <>
+      <tr>
+        <td>{codigo_producto}</td>
+        <td>{nombre_producto}</td>
+        <td>{precio_unitario}</td>
+        <td>{cantidad}</td>
+        <td>{precio_unitario * cantidad}</td>
+        <td>
+          <img src={eliminar} alt='' />
+        </td>
+      </tr>
+      <tr>
+        <td>TOTAL:</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>150000</td>
+      </tr>
+    </>
   );
 };
